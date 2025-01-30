ea6e04fe547a981055c504040ac8f6387fdcc7da
DELL <DELL@192.168.68.102>
2025-01-30 12:52:16 +0530

    firstcommit

--- src/test/java/inspage/ins_page.java
+++ src/test/java/inspage/ins_page.java
@@ -1,108 +1,150 @@
+
 package inspage;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.JavascriptExecutor;
 
 public class ins_page {
-WebDriver driver;
-	
-	By getquote =By.xpath("//body/main[@id='app']/section[1]/div[1]/span[1]/a[1]");
+    WebDriver driver;
 
-	By carMakeDropdown = By.xpath("1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[2]/span[1]/div[1]/div[1]/input[1]"); 
-   By carModelDropdown = By.id("react-autowhatever-Motor-capturedData-carInformation-VehicleMakeModelTrimEngineFlexdata--item-22"); 
-    By manufactureYearDropdown = By.xpath("//*[contains(@id, 'VehicleYearNewFlexdata')]/div/select\""); 
-    By yesoption=By.xpath("\"//label[text()='Yes");
-    
-By emaratesdrop=By.id("sk-Motor-capturedData-carInformation-placeOfRegistration");
-By emarated=By.xpath("//*[@id=\"sk-Motor-capturedData-carInformation-placeOfRegistration\"]/div[2]/div/div/div[1]/div[1]");
-    By registrationdate = By.id("//*[@id='registrationDateElement']\")"); 
-    By vehicleUsageDropdown = By.id("vehicleUsage"); 
+    By getquote = By.xpath("//body/main[@id='app']/section[1]/div[1]/span[1]/a[1]");
+    By carMakeInput = By.xpath("//*[@id='sk-Motor-capturedData-carInformation-VehicleMakeModelTrimEngineFlexdata']/div[2]/span/div/div[1]/input");
+    By carModelInput = By.xpath("//*[@id='sk-navigation-CreateMotorQuoteProcessVehicleUIDef-FieldsetGroup1-car1']");
+    By manufactureYearDropdown = By.xpath("//*[contains(@id, 'VehicleYearNewFlexdata')]/div/select");
+    By yesOption = By.xpath("//*[@id='sk-Motor-capturedData-carInformation-isCarGccSpecAndNonModified']/div[2]/div/span[1]/label/div");
+    By emaratesDropdown = By.id("sk-Motor-capturedData-carInformation-placeOfRegistration");
+    By emiratesOption = By.xpath("//*[@id='sk-Motor-capturedData-carInformation-placeOfRegistration']/div[2]/div/div/div[1]/div[1]");
+    By registrationDate = By.id("registrationDateElement");
+    By vehicleUsageDropdown = By.id("vehicleUsage");
     By submitButton = By.id("submitQuote");
 
-
-public ins_page (WebDriver driver)
-{
-	this.driver=driver;
-	
-    PageFactory.initElements(driver, this);
-}
-public void firstpage()
-{
-	driver.findElement(getquote).click();
-	
-}
-
-public void selectCarMake(String make) 
-{
-   Select select = new Select(driver.findElement(carMakeDropdown));
-    select.selectByVisibleText(make);
-}
-
-public void selectCarModel(String model) {
-   Select select = new Select(driver.findElement(carModelDropdown));
-    select.selectByVisibleText(model);
-}
-public void selectManufactureYear(String year) {
-    Select select = new Select(driver.findElement(manufactureYearDropdown));
-    select.selectByVisibleText(year);
-}
-public void selectoption(String type)
-{
-	Select select=new Select(driver.findElement(yesoption));
-	select.selectByVisibleText(type);
-	driver.findElement(yesoption).click();
-	
-}
-public void selectEmarates()
-{
-	
-	driver.findElement(emaratesdrop).click();
-	
-	
-}
-public void selectEmarate(String emar)
-{
-	Select select=new Select(driver.findElement(emarated));
-	select.selectByVisibleText(emar);
-	
-	
-}
-
-//public void enterRegistrationdate(String regNumber) {
-    //driver.findElement(registrationdate).sendKeys(regNumber);
-//}
-public String selecteddate(String string) {
-    String registrationDateText = ((WebElement) registrationdate).getText();
-
-    // Print the raw extracted date (for debugging purposes)
-    System.out.println("Raw Registration Date: " + registrationDateText);
-
-  
-    try {
-        SimpleDateFormat inputFormat = new SimpleDateFormat("dd/MM/yyyy");
-        Date date = inputFormat.parse(registrationDateText);
-
-        // Format the date to "MM-yyyy"
-        SimpleDateFormat outputFormat = new SimpleDateFormat("MM-yyyy");
-        return outputFormat.format(date);
-    } catch (Exception e) {
-        e.printStackTrace();
-        return "Invalid Date Format";  
+    public ins_page(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
     }
-}
 
-public void selectVehicleUsage(String usage) {
-    Select select = new Select(driver.findElement(vehicleUsageDropdown));
-    select.selectByVisibleText(usage);
-}
+    public void firstpage() {
+        driver.findElement(getquote).click();
+    }
 
-public void clickSubmit() {
-    driver.findElement(submitButton).click();
-}
+    public void selectCarMake(String make) {
+        WebElement carMake = driver.findElement(carMakeInput);
+        carMake.sendKeys(make);
+    }
+
+   /* public void selectCarModel(String model) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        WebElement carModel = wait.until(ExpectedConditions.elementToBeClickable(carModelInput));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", carModel);
+        carModel.sendKeys(model);
+        System.out.println("Entered Car Model: " + model);
+
+        WebElement carModelOption = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[contains(@class,'dropdown-item') and text()='" + model + "']")));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", carModelOption);
+        System.out.println("Selected Car Model from dropdown: " + model);
+    }*/
+   /* public void selectCarModel(String model) throws Exception  {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(40));
+
+        try {
+            // Click to open dropdown
+            WebElement carModelField = driver.findElement(carModelInput);
+            carModelField.click();
+            Thread.sleep(2000);  // Small wait to allow dropdown to load
+
+            // Type the model name to filter results
+            carModelField.sendKeys(model);
+            Thread.sleep(2000);  
+
+            // Wait for the model option to appear
+            WebElement carModelOption = wait.until(ExpectedConditions.presenceOfElementLocated(
+                By.xpath("//div[contains(@class,'dropdown-item') and contains(text(),'" + model + "')]")
+            ));
+
+            // Scroll to the option and click
+            ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", carModelOption);
+            carModelOption.click();
+
+            System.out.println("Selected Car Model: " + model);
+        } catch (Exception e) {
+            System.out.println("Error selecting car model: " + e.getMessage());
+            throw e;  
+        }
+    }*/
+ // Method to select car model
+    public void selectCarModel(String model) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        
+        // Wait for the dropdown to be visible and clickable
+        WebElement carModelDropdown = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='sk-navigation-CreateMotorQuoteProcessVehicleUIDef-FieldsetGroup1-car1']"))); // Replace with actual XPath
+        
+        // Scroll the element into view if it's not already in view
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", carModelDropdown);
+
+        // Now try to interact with the element
+        try {
+            carModelDropdown.sendKeys(model);
+            System.out.println("Car model selected: " + model);
+        } catch (Exception e) {
+            System.out.println("Error selecting car model: " + e.getMessage());
+        }
+    }
+
+    public void selectManufactureYear(String year) {
+        WebElement yearDropdown = driver.findElement(manufactureYearDropdown);
+        yearDropdown.sendKeys(year);
+    }
+
+    public void selectoption(String type) {
+        if (type.equalsIgnoreCase("yes")) {
+            driver.findElement(yesOption).click();
+        }
+    }
+
+    public void selectEmarates() {
+        driver.findElement(emaratesDropdown).click();
+    }
+
+    public void selectEmarate(String emirate) {
+        WebElement emirateOption = driver.findElement(By.xpath("//div[text()='" + emirate + "']"));
+        emirateOption.click();
+    }
+
+    public void enterRegistrationdate(String regNumber) {
+        WebElement regDateField = driver.findElement(registrationDate);
+        regDateField.sendKeys(regNumber);
+    }
+
+    public String selecteddate(String string) {
+        WebElement regDateField = driver.findElement(registrationDate);
+        String registrationDateText = regDateField.getAttribute("value");
+        try {
+            SimpleDateFormat inputFormat = new SimpleDateFormat("dd/MM/yyyy");
+            Date date = inputFormat.parse(registrationDateText);
+            SimpleDateFormat outputFormat = new SimpleDateFormat("MM-yyyy");
+            return outputFormat.format(date);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Invalid Date Format";
+        }
+    }
+
+    public void selectVehicleUsage(String usage) {
+        WebElement usageDropdown = driver.findElement(vehicleUsageDropdown);
+        usageDropdown.sendKeys(usage);
+    }
+
+    public void clickSubmit() {
+        driver.findElement(submitButton).click();
+    }
 }
--- src/test/java/instest/ins_test.java
+++ src/test/java/instest/ins_test.java
@@ -1,67 +1,105 @@
 package instest;
 
-
-
-
 import java.time.Duration;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
 import inspage.ins_page;
 
-
-
-
 public class ins_test {
-	WebDriver driver;
-	@BeforeTest
-	public void setup()
-	{
-		driver =new ChromeDriver();
-		
-	}
-@BeforeMethod
-public void openurl()
-{
-	driver.get("https://protego.ae/home-page.html");
-	driver.manage().window().maximize();
-	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+    WebDriver driver;
+    ins_page ob; // Page Object instance
 
+    @BeforeTest
+    public void setup() {
+        try {
+            driver = new ChromeDriver();
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+            ob = new ins_page(driver); // Initialize Page Object
+        } catch (Exception e) {
+            System.out.println("Error in setup: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    @BeforeMethod
+    public void openurl() {
+        try {
+            driver.get("https://protego.ae/home-page.html");
+        } catch (Exception e) {
+            System.out.println("Error while opening the URL: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testInsuranceForm() {
+        try {
+            System.out.println("Starting test: Insurance Quote Submission");
+
+            // Click 'Get Quote'
+            ob.firstpage();
+            System.out.println("Clicked 'Get Quote' button.");
+
+            // Select car details
+            ob.selectCarMake("Ford");
+            System.out.println("Entered Car Make: Ford");
+
+            ob.selectCarModel("FORD B-MAX STANDARD 1.4 L");
+            System.out.println("Selected Car Model: FORD B-MAX STANDARD 1.4 L");
+
+            ob.selectManufactureYear("2022");
+            System.out.println("Selected Manufacture Year: 2022");
+
+            // Select GCC Spec
+            ob.selectoption("yes");
+            System.out.println("Selected GCC Spec: Yes");
+
+            // Select Emirates
+            ob.selectEmarates();
+            ob.selectEmarate("Sharjah");
+            System.out.println("Selected Emirates: Sharjah");
+        
+
+           // Enter and verify registration date
+            String formattedDate = ob.selecteddate("27/01/2025");
+            System.out.println("Formatted Registration Date (MM-yyyy): " + formattedDate);
+
+            // Select vehicle usage
+            ob.selectVehicleUsage("Personal");
+            System.out.println("Selected Vehicle Usage: Personal");
+
+            // Submit the form
+            ob.clickSubmit();
+            System.out.println("Clicked Submit button.");
+
+            // Verification (Example: Check if the success message appears)
+            String currentURL = driver.getCurrentUrl();
+            Assert.assertTrue(currentURL.contains("success"), "Quote submission failed!");
+            System.out.println("Test Passed: Quote submitted successfully.");
+        } catch (Exception e) {
+            System.out.println("Test execution failed: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    @AfterTest
+    public void teardown() {
+        try {
+            if (driver != null) {
+                driver.quit();
+                System.out.println("Browser closed.");
+            }
+        } catch (Exception e) {
+            System.out.println("Error during teardown: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
 }
-@Test
-public void first()
-{
-	//object creation
-	 ins_page ob=new ins_page(driver);
-	 ob.firstpage();
-	 ob.selectCarMake("Ford");
-     ob.selectCarModel("FORD ECOSPORT TRENT 1.5L");
-    ob.selectManufactureYear("2022");
-   ob. selectoption("yes");
-   ob.selectEmarates();
-   ob.selectEmarate("sharja");
-   
-   String formattedDate = ob.selecteddate("27/01/2025");
-   System.out.println("Formatted Registration Date (MM-yyyy): " + formattedDate);
-  
- 
-     ob.selectVehicleUsage("Personal");
-
-     // Submit the form
-     ob.clickSubmit();
-
-     //Verification (update the logic based on the actual website behavior)
-    String currentURL = driver.getCurrentUrl();
-   Assert.assertTrue(currentURL.contains("success"), "Quote submission failed!");
- }
- 
- }	 
-	
-	
-
+    
 
